bbefb6b25a90923c14130f53153142c021b38f7d
Timur Kenzhaev <st.kenzhaev@gmail.com>
2026-02-15 17:50:44 +0300

    [feat] adding routing and error page

---
+++ pages/post/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Страница поста {{ id }}</div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+const id = ref(route.params.id);
+</script>
+
+<style></style>
