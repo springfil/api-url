37b3cae03876fe4d7ec0532786788617cb9f9e96
Timur Kenzhaev <st.kenzhaev@gmail.com>
2026-02-16 21:59:25 +0300

    [feat] adding default layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     css: ["~/assets/styles/main.css"],
+    icon: {
+        customCollections: [
+            {
+                prefix: "icons",
+                dir: "./assets/icons",
+            },
+        ],
+    },
+    modules: ["@nuxt/icon"],
 });
